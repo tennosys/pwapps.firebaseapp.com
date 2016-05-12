510da9ccd59b9c993597d811e0af917d5269fddc
Teno Siswono <tennosys@gmail.com>
2016-05-12 14:19:28 +0700

    Update travis-runner.sh

--- travis-runner.sh
+++ travis-runner.sh
@@ -3,25 +3,12 @@ set -o pipefail
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" = "5.1" ]
 then
-  git config --global user.email "samccone@gmail.com"
-  git config --global user.name "auto deployer"
+  git config --global user.email "tennosys@gmail.com"
+  git config --global user.name "tennosys"
 
   # Stamp index.html with the date and time of PSK's deploying
   date_value=`date`
   sed -i.tmp1 "s/This is another card./This is another card. PSK Deployed on: $date_value/" app/index.html
-
-  deploy_ghpages () {
-    # Deploying to GitHub Pages! (http://polymerelements.github.io/polymer-starter-kit)
-    echo Deploying to GitHub Pages
-    sed -i.tmp "s/\/\/ app.baseUrl = '\/polymer-starter-kit/app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js
-    sed -i.tmp2 "s/<\/head>/\  \<script>'https:'!==window.location.protocol\&\&(window.location.protocol='https')<\/script>&/g" app/index.html
-    gulp build-deploy-gh-pages
-    # Undoing Changes to PSK for GitHub Pages
-    cp app/scripts/app.js.tmp app/scripts/app.js
-    rm app/scripts/app.js.tmp
-    cp app/index.html.tmp2 app/index.html
-    rm app/index.html.tmp2
-  }
 
   deploy_firebase () {
     # Deploying to Firebase! (https://polymer-starter-kit.firebaseapp.com)
@@ -42,7 +29,6 @@ then
     rm firebase.json
   }
 
-  deploy_ghpages
   deploy_firebase
 
   # Revert to orginal index.html and delete temp file
